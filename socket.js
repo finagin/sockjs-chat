0e5c2d31f6f16679fc20abdaa9d3e069fb6dbd00
Igor Finagin <Igor@Finag.in>
2018-10-30 16:54:37 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,54 +1,54 @@
 var http = require('http'),
-	sockjs = require('sockjs'),
-	server = http.createServer(),
-	webSockets,
-	buffer = [],
-	clients = {};
+    sockjs = require('sockjs'),
+    server = http.createServer(),
+    webSockets,
+    buffer = [],
+    clients = {};
 
-function whisper (id, message) {
-	if ( !clients[id] ) return;
+function whisper(id, message) {
+    if (!clients[id]) return;
 
-	clients[id].write( JSON.stringify(message) );
+    clients[id].write(JSON.stringify(message));
 }
 
-function broadcast (message, exclude) {
-	for ( var i in clients ) {
-		if ( i != exclude ) clients[i].write( JSON.stringify(message) );
-	}
+function broadcast(message, exclude) {
+    for (var i in clients) {
+        if (i != exclude) clients[i].write(JSON.stringify(message));
+    }
 }
 
-function onConnection (conn) {
-	clients[conn.id] = conn;
+function onConnection(conn) {
+    clients[conn.id] = conn;
 
-	broadcast({ type: 'newUser' }, conn.id);
-	whisper(conn.id, { type: 'history', message: buffer, id: conn.id });
+    broadcast({type: 'newUser'}, conn.id);
+    whisper(conn.id, {type: 'history', message: buffer, id: conn.id});
 
-	conn.on('data', function onDataCB (data) {
-		data = JSON.parse(data);
+    conn.on('data', function onDataCB(data) {
+        data = JSON.parse(data);
 
-		if ( data.type == 'text' ) {
-			if ( !data.message ) return;
+        if (data.type == 'text') {
+            if (!data.message) return;
 
-			data.message = data.message.substr(0, 128);
+            data.message = data.message.substr(0, 128);
 
-			if ( buffer.length > 15 ) buffer.shift();
-			buffer.push(data.message);
+            if (buffer.length > 15) buffer.shift();
+            buffer.push(data.message);
 
-			broadcast({ type: 'message', message: data.message, id: conn.id });
-		}
+            broadcast({type: 'message', message: data.message, id: conn.id});
+        }
 
-		// TODO: add user name
-	});
+        // TODO: add user name
+    });
 
-	conn.on('close', function onCloseCB () {
-		delete clients[conn.id];
+    conn.on('close', function onCloseCB() {
+        delete clients[conn.id];
 
-		broadcast({ type: 'userLeft' });
-	});
+        broadcast({type: 'userLeft'});
+    });
 }
 
 webSockets = sockjs.createServer();
 webSockets.on('connection', onConnection);
 
-webSockets.installHandlers(server, { prefix:'/echo' } );
+webSockets.installHandlers(server, {prefix: '/echo'});
 server.listen(9999, '0.0.0.0');
